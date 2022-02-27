13d94182d8fac79e429532e0fe872ab691d9726c
sungJinKangdev <hour30000@gmail.com>
2022-02-28 04:19:18 +0900

    initialize project.

---
+++ Wind_Vegetation_GOW4/Source/Wind_Vegetation_GOW4/Wind_Vegetation_GOW4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Wind_Vegetation_GOW4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Wind_Vegetation_GOW4, "Wind_Vegetation_GOW4" );
---
+++ Wind_Vegetation_GOW4/Source/Wind_Vegetation_GOW4/Wind_Vegetation_GOW4GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Wind_Vegetation_GOW4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIND_VEGETATION_GOW4_API AWind_Vegetation_GOW4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
