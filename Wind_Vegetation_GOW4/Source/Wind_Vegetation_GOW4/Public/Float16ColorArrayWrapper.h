8d1630fa01886cf4ec46c070f220267df35f564e
sungJinKangdev <hour30000@gmail.com>
2022-03-03 03:11:37 +0900

    implemented UFloat16ColorArrayWrapper class. ( FFloat16Color isn't supported by blueprint )

---
+++ Wind_Vegetation_GOW4/Source/Wind_Vegetation_GOW4/Public/Float16ColorArrayWrapper.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Math/Float16Color.h"
+#include "Float16ColorArrayWrapper.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class WIND_VEGETATION_GOW4_API UFloat16ColorArrayWrapper : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	TArray<FFloat16Color> Array;
+
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	FORCEINLINE int32 Num() const
+	{
+		return Array.Num();
+	}
+
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	FORCEINLINE float R(const int32 index) const
+	{
+		check(index >= 0 && index < Array.Num());
+		return static_cast<float>(Array[index].R);
+	}
+
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	FORCEINLINE float G(const int32 index) const
+	{
+		check(index >= 0 && index < Array.Num());
+		return static_cast<float>(Array[index].G);
+	}
+
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	FORCEINLINE float B(const int32 index) const
+	{
+		check(index >= 0 && index < Array.Num());
+		return static_cast<float>(Array[index].B);
+	}
+
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	FORCEINLINE float A(const int32 index) const
+	{
+		check(index >= 0 && index < Array.Num());
+		return static_cast<float>(Array[index].A);
+	}
+};
